a48078142ce2cd2cb8cda59acbf446ce5b103ea4
Rugile Ma <151032480+EligMaa@users.noreply.github.com>
2025-10-06 00:35:41 +0300

    sujungtas front ir back ir duomenu baze

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(
     cors({
-        origin: 'http://localhost:5173'
+      credentials: true,
+        origin: process.env.CLIENT_URL,
     })
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend server!');
-});
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    secure: "process.env.NODE_ENV" === "production" ? true : "auto",
+    sameSite: "process.env.NODE_ENV" === "production" ? "none" : "lax",
+  },
+  resave: false,
+  saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const indexRouter = require('./routers/indexRouter');
+app.use("/", indexRouter);
+
+const authRouter = require('./routers/authRouter');
+app.use("/auth", authRouter);
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the API!' });
